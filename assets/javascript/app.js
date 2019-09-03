d519aee1aad7df559766f23b3e153017b1e4b70d
Tianna Mosby <mosbyts@vcu.edu>
2019-09-03 18:30:40 -0400

    Fixed timer display.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,32 @@
 var correctAnswers = 0;
 var wrongAnswers = 0;
-var Unaswered = 0
+var Unanswered = 0;
+var timerDisplay = 60;
 
+        //GAME START//
 //Timer will begin counting down
-setTimeout(function(){
+setTimeout(function countdown(){
     window.location.href = 'score.html';
  }, 60000);
 
-    setTimeout();
-//Save user's input
-    //Push number of correct choices to correctAnswers var
-    //Push number of wrong choices to wrongAnswers var
-    //Push number of unanswered to Unanswered var
+//Displays the countdown to user
+function countdownDisplay(){
+    timerDisplay--;
+    $("#timeLeft").text(timerDisplay);
+};
 
+window.setInterval(function(){
+    countdownDisplay();
+}, 1000);
+
+
+        //GAME//
+//Push number of correct choices to correctAnswers var
+//Push number of wrong choices to wrongAnswers var
+//Push number of unanswered to Unanswered var
+
+        //GAME ENDS//
 //Displays number of correct, wrong and unanswered
+$("#correctAnswers").text(correctAnswers);
+$("#wrongAnswers").text(wrongAnswers);
+$("#Unanswered").text(Unanswered);
